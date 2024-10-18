afa5280b295c99ed86871817858b62ef0c2eee04
Cyril Dutrieux <cyril.dutrieux@gmail.com>
2024-10-18 19:42:31 +0200

    am2320: Read temp and humidity on one time

--- am2320.py
+++ am2320.py
@@ -34,9 +34,14 @@ class AM2320:
         if crc != read_crc:
             raise Exception(f'Bad CRC ({hex(crc)} != {hex(read_crc)})')
 
-    def get_temperature(self):
-        write_request = i2c_msg.write(self.DEVICE_ADDRESS, [0x03, 0x02, 0x02])
-        read_request = i2c_msg.read(self.DEVICE_ADDRESS, 6)
+    def _get_shorts(self, index, size):
+        ''' Read bytes at specified index and shape short integers
+            
+            index: start address
+            size: number of bytes to read (twice the numer of shorts to expect)
+        '''
+        write_request = i2c_msg.write(self.DEVICE_ADDRESS, [0x03, index, size])
+        read_request = i2c_msg.read(self.DEVICE_ADDRESS, 4+size)
         self.wake_up()
         for _ in range(1):
             try:
@@ -56,14 +61,30 @@ class AM2320:
 
         self._check_crc(data)
 
-        ret = (data[2]*255 + data[3]) / 10
+        # Split result to 2 sized chunks and compute shorts
+        chunks = [data[i:i + 2] for i in range(2, len(data)-2, 2)]
+        shorts = [data[0]*255 + data[1] for data in chunks]
 
-        return ret
+        return shorts
+    
+    def get_both(self):
+        both = self._get_shorts(0, 4)
+        return both[0]/10, both[1]/10
+
+    def get_temperature(self):
+        return self._get_shorts(2, 2)[0] / 10
 
     def get_humidity(self):
-        pass
+        return self._get_shorts(0, 2)[0] / 10
 
 if __name__ == '__main__':
     dev = AM2320()
-    temp = dev.get_temperature()
-    print(temp)
+    # temp = dev.get_temperature()
+    # print(temp)
+    while True:
+        try:
+            hum, temp = dev.get_both()
+            break
+        except:
+            continue
+    print(temp, hum)
