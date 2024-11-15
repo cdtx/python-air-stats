628862d477123c619350989ab3ac91057b3cfbf3
Cyril Dutrieux <cyril.dutrieux@gmail.com>
2024-11-16 00:41:59 +0100

    all: Make this module an infinite async loop

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Read AM2320 temperature sensor with python and push to to MQTT for home assistant (with discovery)
+# python-air-stats
+
+Read air quality sensors with python and push to MQTT for home assistant (with discovery)
+
+## AM2320
+
+For temperature and humidity
+
+# SGP30
+
+For TVOC and eCO2
+
+Uses library at : https://github.com/pimoroni/sgp30-python
--- feu.py
+++ feu.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 import os
-import time
-import traceback
 import configparser
-from paho.mqtt.client import Client as PahoClient
+import asyncio
+import aiomqtt
 
 from pymqtt_hass.items import Device
+
 from am2320 import AM2320
+
 
 current_folder = os.path.dirname(os.path.abspath(__file__))
 CONFIG_FILE = 'config.ini'
@@ -24,58 +25,95 @@ def get_config():
 
     return ret
 
-def run():
-    config = get_config()
+class MQTTDevice:
 
-    # Initialize MQTT client
-    client = PahoClient()
-    # Set the client options
-    client.enable_logger()
-    client.username_pw_set(config['MQTT_USERNAME'], config['MQTT_PASSWORD'])
-    # Connect the mqtt client
-    client.connect(config['MQTT_HOST'], config['MQTT_PORT'])
+    def __init__(self):
+        self.config = get_config()
+        self.client = None
+        self.device = None
+        self.device_topic = None
 
-    # Get the configured home-assistant device
-    device = Device(client, os.path.join(current_folder, PYMQTT_HASS_CONFIG_FILE))
-    # Send the hass's device discovery
-    device.send_discovery()
+        self.boost_status = 0
 
-    if True:
+    async def periodic(self, time_s):
+        while True:
+            self.event_refresh.set()
+            await asyncio.sleep(time_s)
+
+    async def publisher(self):
         temp_sensor = AM2320()
-        go_publish = False
-        for _ in range(10):
-            try:
-                print("Try reading AM2320")
-                humidity, temperature = temp_sensor.get_both()
-                print(temperature, humidity)
-                go_publish = True
-                break
-            except:
-                # Try again
-                time.sleep(1)
-                continue
+        while True:
+            go_publish = False
+            for _ in range(10):
+                try:
+                    print("Try reading AM2320")
+                    humidity, temperature = temp_sensor.get_both()
+                    print(temperature, humidity)
+                    go_publish = True
+                    break
+                except:
+                    # Try again
+                    await asyncio.sleep(1)
+                    continue
 
-        if go_publish:
-            device_topic = device.get_device_topic()
+            if go_publish:
+                device_topic = self.device.get_device_topic()
 
-            print("Publish temperature")
-            topic = '/'.join([
-                device_topic,
-                'temperature',
-            ])
-            client.publish(topic, temperature)
-            client.loop()
+                print("Publish temperature")
+                topic = '/'.join([
+                    device_topic,
+                    'temperature',
+                ])
+                await self.client.publish(topic, temperature)
 
-            print("Publish humidity")
-            topic = '/'.join([
-                device_topic,
-                'humidity',
-            ])
-            client.publish(topic, humidity)
-            client.loop()
-            
-        time.sleep(5)
+                print("Publish humidity")
+                topic = '/'.join([
+                    device_topic,
+                    'humidity',
+                ])
+                await self.client.publish(topic, humidity)
 
+            # Block until event_refresh in fired
+            await self.event_refresh.wait()
+            self.event_refresh.clear()
+
+    async def send_discovery(self):
+         for topic, payload in self.device.discovery_items():
+             await self.client.publish(topic, payload)
+
+    async def main(self):
+        ''' main method
+            Launches tasks
+
+            publisher : collect and publish on MQTT, then wait for an event
+            period: triggers the publish signal
+        '''
+        client_config = {
+            'hostname': self.config['MQTT_HOST'],
+            'port': self.config['MQTT_PORT'],
+            'username': self.config['MQTT_USERNAME'],
+            'password': self.config['MQTT_PASSWORD'],
+        }
+
+        loop = asyncio.get_running_loop() 
+
+        async with aiomqtt.Client(**client_config) as client:
+            self.client = client
+
+            self.device = Device(self.client, os.path.join(current_folder, PYMQTT_HASS_CONFIG_FILE))
+
+            await self.send_discovery()
+
+            self.event_refresh = asyncio.Event(loop=loop) 
+
+            publish_tk = loop.create_task(self.publisher())
+            period_normal_tk = loop.create_task(
+                self.periodic(60)
+            )
+
+            await publish_tk
+            await period_normal_tk
 
 if __name__ == '__main__':
-    run()
+    asyncio.run(MQTTDevice().main())
+
